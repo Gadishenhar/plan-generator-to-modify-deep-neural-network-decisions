11eb3c1924c5221c7b79169136b49b52abc6a0aa
adibenhayun <62426669+adibenhayun@users.noreply.github.com>
2020-04-23 11:13:43 +0300

    Partial rework of preprocessing to work on low-RAM machine

--- preprocessor.py
+++ preprocessor.py
@@ -1,17 +1,37 @@
 import pandas as pd
-import sys
+import os.path
+
+# A full list of the columns in the complete merged dataset, before any columns are dropped
+COL_NAMES = [
+    'LOAN_ID',       'ORIG_CHAN', 'SELLER_NAME',    'ORIG_INT_R',   'ORIG_UPB',
+    'ORIG_LOAN_T',   'ORIG_DATE', 'FST_PAY_DATE',   'ORIG_LTV',     'ORIG_CLTV',
+    'NUM_BOR',       'ORIG_DTIR', 'BOR_C_SCORE',    'FST_TIME_IND', 'LOAN_PURPOSE',
+    'PROP_TYPE',     'NUM_UNITS', 'OCC_TYPE',       'PROP_STATE',   'ZIP_SHORT',
+    'PRIM_INS_PERC', 'PROD_TYPE', 'CO_BOR_C_SCORE', 'INSUR_TYPE',   'RELOC_IND',
+    'DEFAULT'
+]
+
+CLEANED_COL_NAMES =  [
+                     'ORIG_CHAN', 'SELLER_NAME',    'ORIG_INT_R',   'ORIG_UPB',
+    'ORIG_LOAN_T',                                  'ORIG_LTV',     'ORIG_CLTV',
+    'NUM_BOR',       'ORIG_DTIR', 'BOR_C_SCORE',    'FST_TIME_IND', 'LOAN_PURPOSE',
+                     'NUM_UNITS', 'OCC_TYPE',       'PROP_STATE',   'ZIP_SHORT',
+    'PRIM_INS_PERC', 'PROD_TYPE', 'CO_BOR_C_SCORE', 'INSUR_TYPE',   'RELOC_IND',
+    'DEFAULT'
+]
 
 
 def print_useful_info(acq_df_col, col_num):
     return 0
-    #print('Column #', col_num)
-    #print('There are', len(acq_df_col), 'total entries')
-    #print('There are', acq_df_col.nunique(), 'unique values')
-    #print('There are', sum(acq_df_col.isnull()), 'missing values')
-    #print('')
+    print('Processing column', col_num)
+    print('Column #', col_num)
+    print('There are', len(acq_df_col), 'total entries')
+    print('There are', acq_df_col.nunique(), 'unique values')
+    print('There are', sum(acq_df_col.isnull()), 'missing values')
+    print('')
 
 
-def prep_acq_columns(df, COL_NAMES):
+def prep_columns(df, seller_names, property_states):
 
     to_be_deleted = []
 
@@ -31,8 +51,7 @@ def prep_acq_columns(df, COL_NAMES):
     # Column 2 - SELLER NAME
     # TODO Consider using values indicative of prevalence of sellers
     print_useful_info(df.iloc[:, 2], 2)
-    sellers = set(df.iloc[:, 2])
-    for i, seller in enumerate(sellers):
+    for i, seller in enumerate(seller_names):
         df.iloc[:, 2].replace(seller, i, inplace=True)
 
     # Column 3 - ORIGINAL INTEREST RATE
@@ -114,8 +133,7 @@ def prep_acq_columns(df, COL_NAMES):
     # Column 18 - PROPERTY STATE
     # TODO Also sort by how common the values are?
     print_useful_info(df.iloc[:, 18], 18)
-    states = set(df.iloc[:, 18])
-    for i, state in enumerate(states):
+    for i, state in enumerate(property_states):
         df.iloc[:, 18].replace(state, i, inplace=True)
 
     # Column 19 - ZIP CODE SHORT
@@ -154,7 +172,10 @@ def prep_acq_columns(df, COL_NAMES):
     # Drop any leftover entires that contains NaNs (should be a single entry, due to a single NaN in column 9)
     df.dropna(inplace=True)
 
-    return df, to_be_deleted
+    # Delete unneeded columns
+    df.drop(to_be_deleted, axis=1, inplace=True)
+
+    return df
 
 
 def split_train_val_test(df, OUT_PATH, TRAIN_VAL_TEST_SPLIT):
@@ -175,16 +196,11 @@ def split_train_val_test(df, OUT_PATH, TRAIN_VAL_TEST_SPLIT):
     ZEROS_TRAIN_LEN = round(ZEROS_LEN * TRAIN_VAL_TEST_SPLIT[0])
     ONES_TRAIN_LEN = round(ONES_LEN * TRAIN_VAL_TEST_SPLIT[0])
 
-    # The number of zeros should be only 9 times greater than the number of ones
-    #ZEROS_TRAIN_LEN = min(ZEROS_TRAIN_LEN, 2*ONES_TRAIN_LEN)
-
     ZEROS_VAL_LEN = round(ZEROS_LEN * TRAIN_VAL_TEST_SPLIT[1])
     ONES_VAL_LEN = round(ONES_LEN * TRAIN_VAL_TEST_SPLIT[1])
-    #ZEROS_VAL_LEN = min(ZEROS_VAL_LEN, 2 * ONES_VAL_LEN)
 
     ZEROS_TEST_LEN = ZEROS_LEN - ZEROS_TRAIN_LEN - ZEROS_VAL_LEN
     ONES_TEST_LEN = ONES_LEN - ONES_TRAIN_LEN - ONES_VAL_LEN
-    #ZEROS_TEST_LEN = min(ZEROS_TEST_LEN, 2 * ONES_TEST_LEN)
 
     train = zeros_df.iloc[:ZEROS_TRAIN_LEN, :]
     train = train.append(ones_df.iloc[:ONES_TRAIN_LEN, :])
@@ -192,7 +208,7 @@ def split_train_val_test(df, OUT_PATH, TRAIN_VAL_TEST_SPLIT):
 
     PER0 = sum(train.iloc[:, -1] == 0) / len(train) * 100
     PER1 = 100 - PER0
-    #print('The train data set contains ', PER0, 'zeros and ', PER1)
+    print('The train data set contains ', PER0, 'zeros and ', PER1, 'ones')
 
     val = zeros_df.iloc[ZEROS_TRAIN_LEN:(ZEROS_TRAIN_LEN + ZEROS_VAL_LEN), :]
     val = val.append(ones_df.iloc[ONES_TRAIN_LEN:(ONES_TRAIN_LEN + ONES_VAL_LEN), :])
@@ -200,7 +216,7 @@ def split_train_val_test(df, OUT_PATH, TRAIN_VAL_TEST_SPLIT):
 
     PER0 = sum(val.iloc[:, -1] == 0) / len(val) * 100
     PER1 = 100 - PER0
-    #print('The validation data set contains ', PER0, 'zeros and ', PER1)
+    print('The validation data set contains ', PER0, 'zeros and ', PER1, 'ones')
 
     test = zeros_df.iloc[-ZEROS_TEST_LEN:, :]
     test = test.append(ones_df.iloc[-ONES_TEST_LEN:, :])
@@ -208,82 +224,12 @@ def split_train_val_test(df, OUT_PATH, TRAIN_VAL_TEST_SPLIT):
 
     PER0 = sum(test.iloc[:, -1] == 0) / len(test) * 100
     PER1 = 100 - PER0
-    #print('The test data set contains ', PER0, 'zeros and ', PER1)
+    print('The test data set contains ', PER0, 'zeros and ', PER1, 'ones')
 
     return (train, val, test)
 
 
-def preprocess_merged_df(df):
-
-    # Load data set
-    COL_NAMES = [
-        'LOAN_ID',
-        'ORIG_CHAN',
-        'SELLER_NAME',
-        'ORIG_INT_R',
-        'ORIG_UPB',
-        'ORIG_LOAN_T',
-        'ORIG_DATE',
-        'FST_PAY_DATE',
-        'ORIG_LTV',
-        'ORIG_CLTV',
-        'NUM_BOR',
-        'ORIG_DTIR',
-        'BOR_C_SCORE',
-        'FST_TIME_IND',
-        'LOAN_PURPOSE',
-        'PROP_TYPE',
-        'NUM_UNITS',
-        'OCC_TYPE',
-        'PROP_STATE',
-        'ZIP_SHORT',
-        'PRIM_INS_PERC',
-        'PROD_TYPE',
-        'CO_BOR_C_SCORE',
-        'INSUR_TYPE',
-        'RELOC_IND',
-        'DEFAULT'
-    ]
-
-    # First, convert all entries to numerical values, fill missing values and remember which columns should be deleted
-    df, to_be_deleted = prep_acq_columns(df, COL_NAMES)
-
-    # Remove unnecessary columns
-    #print('Removing unnecessary columns...\n')
-    df.drop(to_be_deleted, axis=1, inplace=True)
-
-    # Normalization (note this ignores the labels):
-    #print('Normalizing...\n')
-    features = df.iloc[:, :-1]
-    df.iloc[:, :-1] = (features - features.mean()) / features.std()
-    df.iloc[:, :-1] = (features - features.min()) / (features.max() - features.min())
-
-    print('Splitting to train, validation and test sets...\n')
-    return split_train_val_test(df, OUT_PATH, TRAIN_VAL_TEST_SPLIT)
-
-
-def main(acq_path, per_path, out_path, TRAIN_VAL_TEST_SPLIT):
-
-    # Firstly, go over all of the acquisition files, and merge them with the correct performance data
-    merged_df = pd.DataFrame() #Initialize an empty dataframe
-    for year in range(2000, 2018+1):
-        for quarter in range(1, 4+1):
-            acquisition_file_name = acq_path + '/Acquisition_' +str(year) + 'Q' + str(quarter) + '.txt'
-            performance_file_name = per_path + '/Performance_' +str(year) + 'Q' + str(quarter) + '.txt'
-            print('Currently processing', year, 'quarter', quarter)
-            merged_df.append(merge_aqs_per(per_path, acq_path)) #adding this quarter's united dataset to "merged_df"
-    merged_df.to_csv(OUT_PATH + 'merged'+ '.txt', index=False, header=False)
-
-    # Next, preprocess the single dataframe
-    new_train, new_val, new_test = preprocess_merged_df(merged_df)
-
-    # Shuffle one last time, so that the entry year will not matter, and save the sets
-    new_train.sample(frac=1).to_csv(OUT_PATH + 'train' + '.txt', index=False, header=False) #"sample" takes random chuncks of the data, so we choose a piece in the size of 1, meaning creating randomlization
-    new_val.sample(frac=1).to_csv(OUT_PATH + 'val' + '.txt', index=False, header=False)
-    new_test.sample(frac=1).to_csv(OUT_PATH + 'test' + '.txt', index=False, header=False)
-
-
-def merge_aqs_per(per_path, acq_path):
+def merge_acq_per_data(acq_path, per_path):
 
     # Before we remove the identifier column, we need to process the performance data
     PER_COL_NAMES = ['LOAN_ID', 'DEFAULT'] #Choose two columns from the performance data: the loan's identifier + "DEFAULT": the date in which a costumer's house was confiscated
@@ -295,16 +241,131 @@ def merge_aqs_per(per_path, acq_path):
     per_df.loc[per_df[PER_COL_NAMES[1]] != 0, PER_COL_NAMES[1]] = 1 #in any case there's a result other than 0 it means that the house indeed was confiscated so we write "1"
     per_df.iloc[:, -1] = per_df.iloc[:, -1].astype(int) #Converting to integers
 
-    acq_df = pd.read_csv(acq_path, sep="|")
+    acq_df = pd.read_csv(acq_path, sep="|", names=COL_NAMES[:-1])
 
     # Merge with the acquisition data set, based on the load identifier
     #print('Merging with performance data...\n')
     df = pd.merge(acq_df, per_df, on='LOAN_ID', how='inner') #merge according to Loan indentifier
+    return df
+
+
+def main(acq_path, per_path, out_path, TRAIN_VAL_TEST_SPLIT):
+
+    """
+    # Iteration 1: go over all of the acquisition files, and merge them with the appropriate performance data
+    # If the merged data set files already exist, we can skip this step
+    force_iter_1 = False
+    if (not os.path.isfile(out_path + 'AcqPer_2018Q4.txt')) or force_iter_1:
+        for year in range(2000, 2018+1):
+            for quarter in range(1, 4+1):
+                suffix = str(year) + 'Q' + str(quarter) + '.txt'
+                acquisition_file_name = acq_path + 'Acquisition_' + suffix
+                performance_file_name = per_path + 'Performance_' + suffix
+                print('Merging data from the year', year, 'quarter', quarter, '...')
+                merged_df = merge_acq_per_data(acquisition_file_name, performance_file_name)
+                merged_df.to_csv(out_path + 'AcqPer_' + suffix, index=False, header=False)
+
+    # Iteration 2: go over all of the merged data sets and extract from them all of the unique values from the seller
+    # name and property state columns
+    force_skip_iter_2 = True
+    if not force_skip_iter_2:
+        print('Extracting unique sellers and property states...')
+        seller_names = set()
+        property_states = set()
+        for year in range(2000, 2018+1):
+            for quarter in range(1, 4+1):
+                suffix = str(year) + 'Q' + str(quarter) + '.txt'
+                file_name = out_path + 'AcqPer_' + suffix
+                print('Processing year', year, 'quarter', quarter, '...')
+                seller_names = seller_names.union(set(pd.read_csv(file_name, usecols=[2]).iloc[:, 0].unique()))
+                property_states = property_states.union(set(pd.read_csv(file_name, usecols=[18]).iloc[:, 0].unique()))
+
+        # Now that we have all possible values saved, convert the set to list, to assure deterministic order
+        seller_names = list(seller_names)
+        property_states = list(property_states)
+
+    # Iteration 3: go over each data set file, replace all string values with numerical values, fill missing entries,
+    # drop any remaining NaNs and remove some columns. Data is NOT normalized yet.
+    # If the tokenized data set files already exist, we can skip this step
+    force_iter_3 = False
+    if (not os.path.isfile(out_path + 'TokAcqPer_2018Q4.txt')) or force_iter_3:
+        for year in range(2000, 2018+1):
+            for quarter in range(1, 4+1):
+                suffix = str(year) + 'Q' + str(quarter) + '.txt'
+                file_name = out_path + 'AcqPer_' + suffix
+                print('Tokenizing year', year, 'quarter', quarter, '...')
+                df = pd.read_csv(file_name, names=COL_NAMES)
+                prep_columns(df, seller_names, property_states).to_csv(out_path + 'TokAcqPer_' + suffix, index=False, header=False)
+
+    # The next step must be done manually (for now) outside of python:
+    # * merge all of the text files to one
+    # * save the result of df.describe() to stats.txt
+    # TODO Write code for it here
+
+    # Iter 4: Even though we have a single tokenized file, we need to normalize each quarter separately.
+    if os.path.isfile(out_path + 'stats.txt') and (not os.path.isfile(out_path + 'NormTokAcqPer_2018Q4.txt')):
+        stats_df = pd.read_csv(out_path + 'stats.txt')
+        for year in range(2000, 2018+1):
+            for quarter in range(1, 4+1):
+                suffix = str(year) + 'Q' + str(quarter) + '.txt'
+                file_name = out_path + 'TokAcqPer_' + suffix
+                print('Normalizing year', year, 'quarter', quarter, '...')
+                df = pd.read_csv(file_name, names=CLEANED_COL_NAMES)
+                # .values.squeeze() because stack overflow says so
+                features_mean = stats_df.loc[1][1:-1].astype(float).values.squeeze()  # mean shows up in the second row of describe()
+                features_std = stats_df.loc[2][1:-1].astype(float).values.squeeze()  # std is third row
+                features_min = stats_df.loc[3][1:-1].astype(float).values.squeeze()  # min is fourth row
+                features_max = stats_df.loc[7][1:-1].astype(float).values.squeeze()  # max is eighth row
+                df.iloc[:, :-1] = (df.iloc[:, :-1] - features_mean) / features_std
+                df.iloc[:, :-1] = (df.iloc[:, :-1] - features_mean) / (features_max - features_min)
+                df.to_csv(out_path + 'NormTokAcqPer_' + suffix, index=False, header=False)
+    """
+
+    # Iter 5: Next, we need to go over each normalized tokenized data set file, and split it to a train file, a
+    # validation file and a test file, using the given split.
+    if os.path.isfile(out_path + 'NormTokAcqPer_2018Q4.txt'):
+        for year in range(2000, 2018+1):
+            for quarter in range(1, 4+1):
+                suffix = str(year) + 'Q' + str(quarter) + '.txt'
+                file_name = out_path + 'NormTokAcqPer_' + suffix
+                print('Splitting year', year, 'quarter', quarter, '...')
+                df = pd.read_csv(file_name, names=CLEANED_COL_NAMES)
+                train, val, test = split_train_val_test(df, OUT_PATH, TRAIN_VAL_TEST_SPLIT)
+                train.to_csv(OUT_PATH + 'train' + suffix, index=False, header=False)
+                train.to_csv(OUT_PATH + 'val' + suffix, index=False, header=False)
+                train.to_csv(OUT_PATH + 'test' + suffix, index=False, header=False)
+
+    #
+    #
+    # save data about them that we will need later.
+    # The things saved in this iteration are:
+    # * An aggregate list of all of the unique values (for the seller name and property state columns), as well as
+    #   their popularity in the data set. This is done so we can give these non-numerical values consistent values.
+
+
+    """
+
+    # Merge all of newly created files to a single file, and delete all of the old files
+    # TODO Do this in code. Currently, done manually while debugger is paused
+
+    # Load the merged dataset
+
+    merged_df = pd.read_csv(OUT_PATH + 'merged.txt', names=COL_NAMES)
+    print(merged_df)
+
+    # Next, preprocess the single dataframe
+    new_train, new_val, new_test = preprocess_merged_df(merged_df)
+
+    # Shuffle one last time, so that the entry year will not matter, and save the sets
+    new_train.sample(frac=1).to_csv(OUT_PATH + 'train' + '.txt', index=False, header=False) #"sample" takes random chuncks of the data, so we choose a piece in the size of 1, meaning creating randomlization
+    new_val.sample(frac=1).to_csv(OUT_PATH + 'val' + '.txt', index=False, header=False)
+    new_test.sample(frac=1).to_csv(OUT_PATH + 'test' + '.txt', index=False, header=False)
+    """
 
 
 if __name__ == '__main__':
-    ACQ_PATH = 'Dataset/acquisition'
-    PER_PATH = 'Dataset/performance'
+    ACQ_PATH = 'Dataset/acquisition/'
+    PER_PATH = 'Dataset/performance/'
     OUT_PATH = 'Dataset/prep_unbiased/'
     TRAIN_VAL_TEST_SPLIT = [0.6, 0.2, 0.2]
     main(ACQ_PATH, PER_PATH, OUT_PATH, TRAIN_VAL_TEST_SPLIT)
